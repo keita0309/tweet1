fbbcd2330af4119a5443ce985860a036a4165dc7
keita0309 <kta0309@gmail.com>
2019-12-07 16:50:55 +0900

    ヘッダーマークアップ

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,16 @@
 
 ActiveRecord::Schema.define(version: 2019_12_05_141206) do
 
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
